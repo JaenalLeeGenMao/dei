236d31f2b28fda40255b5479f47ef6bec288bd62
jaenal <jaenal.leegenmao@gmail.com>
2017-07-27 14:26:21 +0700

    seperate the file into classes like OO

--- parking_lot/main.py
+++ parking_lot/main.py
@@ -1,15 +1,6 @@
 from __future__ import print_function
+from parking_lot import *
 
-def create_parking_slot(n):
-
-    n = int(n)
-    # Verify if input is at least 2 inputs
-    if (n < 2):
-        return "Please input a list greater or equal 2"
-
-    # create parking slot list and return the list
-    parking_slot = [None] * n
-    return parking_slot
 
 def read_file(file_name):
 
@@ -24,97 +15,12 @@ def read_file(file_name):
 
     return collection
 
-def assign_parking(parking_slot, current_car):
-
-    # looping through the parking slot to find a slot for current car
-    for each_slot in range(len(parking_slot)):
-        # if slot is empty, successfully allocated to a slot then return index of the allocated slot
-        if (parking_slot[each_slot] == None):
-
-            parking_slot[each_slot] = current_car
-            return each_slot
-
-    # if no slots are available return -1
-    return -1
-
-def remove_parking(parking_slot, index):
-
-    index = int(index) - 1
-    # Verify if input is at least 2 inputs
-    if ( index < 0 and index >= len(parking_slot)):
-
-        print("Parking slot index is out of range")
-        return "Parking slot index is out of range"
-
-    if ( parking_slot[index] == None):
-
-        print("Slot number ", index + 1, " is already free")
-
-    else:
-
-        parking_slot[index] = None
-        print("Slot number ", index + 1, " is free")
-
-def status(parking_slot):
-
-    print("Output (tab delimited output):")
-
-    print("Slot No \tRegistration No. \tColour")
-
-    # prints out parking status
-    for index, each in enumerate(parking_slot):
-
-        if (each == None):
-
-            continue
-
-        else:
-
-            print(index + 1, each[1], each[2])
-
-def registration_numbers_for_cars_with_colour(parking_slot, colour):
-
-    for i, each in enumerate(parking_slot):
-
-        if (each[0] == None):
-
-            continue
-
-        elif (each[2] == colour):
-
-            print(each[1], end=" " )
-    print()
-
-def slot_numbers_for_cars_with_colour(parking_slot, colour):
-
-    for i, each in enumerate(parking_slot):
-
-        if (each[0] == None):
-
-            continue
-
-        elif (each[2] == colour):
-
-            print(i + 1, end=" " )
-    print()
-
-def slot_number_for_registration_number(parking_slot, registry_no):
-
-    for i, each in enumerate(parking_slot):
-
-        if (each[0] == None):
-
-            continue
-
-        elif (each[1] == registry_no):
-
-            print(i + 1, end=" " )
-
-    return("Not Found")
-
 def main():
+
     data = read_file("./file_inputs.txt")
     parking_slot = []
+    parking_lot = Parking_lot()
+
     for current in data:
 
         info = current[0]
@@ -122,11 +28,12 @@ def main():
         if (info == "create_parking_lot"):
 
             n = current[1]
-            parking_slot = create_parking_slot(n)
+            parking_slot = parking_lot.create_parking_slot(n)
+            print("Created a parking lot with " + str(len(parking_slot)) + " slots")
 
         elif (info == "park"):
 
-            parking_info = assign_parking(parking_slot, current)
+            parking_info = parking_lot.assign_parking(parking_slot, current)
 
             if (parking_info == -1):
                 print("Sorry, parking lot is full")
@@ -136,23 +43,25 @@ def main():
         elif (info == "leave"):
 
             index = current[1]
-            remove_parking(parking_slot, index)
+            parking_lot.remove_parking(parking_slot, index)
 
         elif (info == "status"):
-            status(parking_slot)
+            parking_lot.status(parking_slot)
 
         elif (info == "registration_numbers_for_cars_with_colour"):
             colour = current[1]
-            registration_numbers_for_cars_with_colour(parking_slot, colour)
+            parking_lot.registration_numbers_for_cars_with_colour(parking_slot, colour)
 
         elif (info == "slot_numbers_for_cars_with_colour"):
             colour = current[1]
-            slot_numbers_for_cars_with_colour(parking_slot, colour)
+            parking_lot.slot_numbers_for_cars_with_colour(parking_slot, colour)
 
         elif (info == "slot_number_for_registration_number"):
             registry_no = current[1]
-            slot_number_for_registration_number(parking_slot, registry_no)
-        # print(current[1:])
+            parking_lot.slot_number_for_registration_number(parking_slot, registry_no)
+
+    # close the parking state
+    parking_lot.close()
 
 # Unit testing
 # Create a parking lot with 6 slot
---
+++ parking_lot/parking_lot.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+
+# global variable for writing each statement into STDOUT.txt
+stdout = open("STDOUT.txt", "w")
+
+class Parking_lot:
+
+    def __init_(self):
+        pass
+
+    def create_parking_slot(self, n):
+
+        n = int(n)
+        # Verify if input is at least 2 inputs
+        if (n < 2):
+            stdout.write("Please input a list greater or equal 2\n")
+            return "Please input a list greater or equal 2"
+
+        # create parking slot list and return the list
+        parking_slot = [None] * n
+        stdout.write("Created a parking lot with " + str(len(parking_slot)) + " slots\n")
+        return parking_slot
+
+    def assign_parking(self, parking_slot, current_car):
+
+        # looping through the parking slot to find a slot for current car
+        for each_slot in range(len(parking_slot)):
+            # if slot is empty, successfully allocated to a slot then return index of the allocated slot
+            if (parking_slot[each_slot] == None):
+
+                parking_slot[each_slot] = current_car
+                stdout.write("Allocated slot number: " + str(each_slot + 1) + "\n")
+                return each_slot
+
+        # if no slots are available return -1
+        stdout.write("Sorry, parking lot is full\n")
+        return -1
+
+    def remove_parking(self, parking_slot, index):
+
+        index = int(index) - 1
+        # Verify if input is at least 2 inputs
+        if ( index < 0 and index >= len(parking_slot)):
+
+            print("Parking slot index is out of range")
+            stdout.write("Parking slot index is out of range\n")
+            return "Parking slot index is out of range"
+
+        if ( parking_slot[index] == None):
+
+            print("Slot number ", index + 1, " is already free")
+            stdout.write("Slot number " + str(index + 1) + " is already free\n")
+
+        else:
+
+            parking_slot[index] = None
+            print("Slot number ", index + 1, " is free")
+            stdout.write("Slot number " + str(index + 1) + " is free\n")
+
+    def status(self, parking_slot):
+
+        print("Output (tab delimited output):")
+        stdout.write("Output (tab delimited output):" + "\n")
+
+        print("Slot No \tRegistration No. \tColour")
+        stdout.write("Slot No \tRegistration No. \tColour" + "\n")
+
+        # prints out parking status
+        for index, each in enumerate(parking_slot):
+
+            if (each == None):
+                # if each is None then skip to next element
+                continue
+
+            else:
+
+                print(index + 1, each[1], each[2])
+                stdout.write(str(index + 1) + " " + str(each[1]) + " " + str(each[2]) + "\n")
+
+    def registration_numbers_for_cars_with_colour(self, parking_slot, colour):
+
+        for i, each in enumerate(parking_slot):
+
+            if (each[0] == None):
+                # if each is None then skip to next element
+                continue
+
+            elif (each[2] == colour):
+
+                print(each[1], end=" " )
+                stdout.write(str(each[1]) + " " )
+        stdout.write("\n" )
+        print()
+
+    def slot_numbers_for_cars_with_colour(self, parking_slot, colour):
+
+        for i, each in enumerate(parking_slot):
+
+            if (each[0] == None):
+                # if each is None then skip to next element
+                continue
+
+            elif (each[2] == colour):
+
+                print(i + 1, end=" " )
+                stdout.write(str(i + 1) + " " )
+        stdout.write("\n" )
+        print()
+
+    def slot_number_for_registration_number(self, parking_slot, registry_no):
+
+        count = 0
+
+        for i, each in enumerate(parking_slot):
+
+            if (each[0] == None):
+                # if each is None then skip to next element
+                continue
+
+            elif (each[1] == registry_no):
+
+                print(i + 1, end=" " )
+                count += 1
+                stdout.write(str(i + 1) + " " )
+
+        # if no item match then return Not Found
+        if (count == 0):
+            stdout.write("\nNot Found")
+            return("Not Found")
+
+    def close(self):
+
+        # close the file to be written into STDOUT.txt
+        stdout.close()
+        return True
